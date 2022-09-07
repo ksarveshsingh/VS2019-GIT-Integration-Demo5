f5da5301bb6c57207602af4024913dce39cec85c
Sarvesh Singh <sarvesh.singh@lseg.com>
2022-09-07 19:06:32 +0530

    Add project files.

---
+++ VS2019-GIT-Integration-Demo5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2019_GIT_Integration_Demo5.Global" Language="C#" %>
---
+++ VS2019-GIT-Integration-Demo5/PreferredLanguage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VS2019_GIT_Integration_Demo5
+{
+    public class PreferredLanguage
+    {
+        public PreferredLanguage()
+        {
+
+        }
+
+        public string MyPreferred()
+        {
+            return "Visual Basic";
+        }
+    }
+}
