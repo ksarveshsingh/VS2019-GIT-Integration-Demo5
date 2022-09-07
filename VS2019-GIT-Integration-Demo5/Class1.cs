8cb0cec2a180f3e38d557c68a548fd8e5d3f3d3f
Sarvesh Singh <sarvesh.singh@lseg.com>
2022-09-07 19:45:47 +0530

    added class file

---
+++ VS2019-GIT-Integration-Demo5/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VS2019_GIT_Integration_Demo5
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+}
